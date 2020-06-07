23f26432e4fed88a8f28f073cc320d82ab17ee59
Ryan Torrecampo <ryantorrecampo@Ryans-MacBook-Pro.local>
2020-06-06 22:50:28 -0500

    CRA init, added register/login to api

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
+
+connectDB();
+
+app.use(express.json({ extended: false }));
+
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
 
 const PORT = process.env.port || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-app.get("/", (req, res) => res.send("works"));
